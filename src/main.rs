f3b56c8650981284d2fb66173d4caa5bbff3e8cb
Yakir Gubi <yakirgubi@gmail.com>
2026-03-16 18:10:58 +0200

    replace unwrap with expect

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use std::io::{self, Write};
 fn main() {
     println!("Hi we in Rusty take care of you :)");
     print!("Please enter your name: ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush().expect("Faild to print!");
 
     let mut user_name = String::new();
-    io::stdin().read_line(&mut user_name).unwrap();
+    io::stdin()
+        .read_line(&mut user_name)
+        .expect("Faild to get name from user!");
 
     println!("Hi {} we are glad to work with you ;)", user_name.trim());
 }
